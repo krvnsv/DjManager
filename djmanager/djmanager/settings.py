4b4850b8ddfe89d7d5f09e424eb5d42907f08990
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-03-31 21:00:32 +0200

    Create custom user model

--- djmanager/djmanager/settings.py
+++ djmanager/djmanager/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'account.User'
+
 
 # Application definition
 
@@ -37,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
     'core',
+    
 ]
 
 MIDDLEWARE = [
