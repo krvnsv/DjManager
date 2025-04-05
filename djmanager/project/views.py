38bdca5af243b2aa2492c1965114026655ae7c4b
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-04-05 15:29:32 +0200

    Project detail page

--- djmanager/project/views.py
+++ djmanager/project/views.py
@@ -6,10 +6,19 @@ from .models import Project
 @login_required
 def projects(request):
     projects = Project.objects.filter(created_by=request.user)
+
     return render(request, 'project/projects.html',{
         'projects': projects
     })
 
+
+@login_required
+def project(request, pk):
+    project = Project.objects.filter(created_by=request.user).get(pk=pk)
+
+    return render(request, 'project/project.html',{
+        'project': project
+    })
 
 @login_required
 def add_project(request):
